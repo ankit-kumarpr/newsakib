2ca11283e08964e3c031e93ac163774fe1227268
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2026-01-07 20:03:07 +0530

    Update config.jsx

--- frontend/src/Pages/config.jsx
+++ frontend/src/Pages/config.jsx
@@ -1,3 +1,3 @@
-const Base_url = "http://localhost:3000/api";
-// const Base_url = "https://newsakib.onrender.com/api";
+// const Base_url = "http://localhost:3000/api";
+const Base_url = "https://newsakib.onrender.com";
 export default Base_url;
